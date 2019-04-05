494eeab0419186b7b46349b09fbf346b7d19081a
Francis Chen <fncis.chen@gmail.com>
2019-04-05 11:08:23 -0700

    outlined App structure for React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,27 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Login from "./Components/login.js";
+import Register from "./Components/register.js";
+import Jokes from "./Components/Jokes.js";
+import Home from "./Components/home.js";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 class App extends Component {
+
+  constructor(props) {
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Sign In</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/jokes">Users</Link> 
+        </nav>
+
+
+        <Route path="/" exact Component={Home}/>
+        <Route path="/login" Component={Login}/>
+        <Route path="/register" Component={Register}/>
+        <Route path="/jokes" Component={Jokes}/> 
       </div>
     );
   }
 }
+
 
 export default App;
